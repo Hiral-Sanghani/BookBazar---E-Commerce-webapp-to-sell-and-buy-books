9110015d6d192f4619fe748417ba41439570787f
Hiral-Sanghani <sanghanihiral1211@gmail.com>
2024-06-06 17:21:19 +0530

    initial commit

---
+++ src/main/java/com/ecommerce/bookbazar/BookbazarApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.bookbazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookbazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookbazarApplication.class, args);
+	}
+
+}
